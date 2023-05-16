fea16c07484f06c5c7bbda1c3e14dd7136da8dd2
Van La <66257620+vanngocthuyla@users.noreply.github.com>
2023-05-16 15:50:12 -0500

    Globally fit mutation Mpro with 3 informative priors

--- global_fitting/mutation/run_me.sh
+++ global_fitting/mutation/run_me.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-export SCRIPT="/home/exouser/python/mpro/scripts/submit_mcmc_informative.py"
+export SCRIPT="/home/vla/python/mpro/scripts/submit_mcmc_informative.py"
 
-export OUT_DIR="/home/exouser/python/mpro/1.normal"
+export OUT_DIR="/home/vla/python/mpro/11.normal"
 
 export N_INTER=10000
 
@@ -10,4 +10,4 @@ export N_BURN=2000
 
 export N_CHAIN=4
 
-python $SCRIPT --out_dir $OUT_DIR --file_name 'normal' --niters $N_INTER --nburn $N_BURN --nchain $N_CHAIN
+python $SCRIPT --out_dir $OUT_DIR --file_name 'informative' --niters $N_INTER --nburn $N_BURN --nchain $N_CHAIN
