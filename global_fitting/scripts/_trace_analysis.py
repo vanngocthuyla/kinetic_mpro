1a9b0bb517bbb92351fec74f9db3051f026fb922
Van La <66257620+vanngocthuyla@users.noreply.github.com>
2023-06-16 22:43:46 -0500

    Update _trace_analysis.py

--- global_fitting/scripts/_trace_analysis.py
+++ global_fitting/scripts/_trace_analysis.py
@@ -85,3 +85,59 @@ def list_kinetics_params(init_params, update_params):
     for name_params in update_params:
         results[name_params] = update_params[name_params]
     return results
+
+
+def extract_params_from_map_and_prior(trace, map_idx, prior_infor):
+    """
+    Parameters:
+    ----------
+    trace        : mcmc.get_samples(group_by_chain=False)
+    map_idx      : index of map from mcmc
+    prior_infor  : list of dict of assigned prior distribution for kinetics parameters
+    
+    Example of prior_infor:
+        [{'type': 'logKd', 'name': 'logKd',  'fit': 'local', 'dist': None, 'value': array([-11, -14])},
+         {'type': 'logK','name': 'logK_S_M','fit': 'global','dist': 'uniform','lower': -20.0,'upper': 0.0}]
+    ----------
+    return two lists of kinetics parameters from map of trace and prior information
+    """
+
+    data = az.convert_to_inference_data(trace)
+    params_name_kcat = []
+    params_name_logK = []
+    for name in trace.keys():
+        if name.startswith('logK'):
+            params_name_logK.append(name)
+        if name.startswith('kcat'):
+            params_name_kcat.append(name)
+
+    params_logK = {}
+    params_kcat = {}
+
+    # Extract params from trace[map_idx]
+    for name in trace.keys():
+        if name in params_name_logK:
+            params_logK[name] = trace[name][map_idx]
+        elif name in params_name_kcat:
+            params_kcat[name] = trace[name][map_idx]
+
+    # Extract params from prior information
+    for prior in prior_infor:
+        if prior['dist'] is None:
+            name = prior['name']
+            if name.startswith('logK'):
+                if prior['fit'] == 'local':
+                    for n in range(len(prior['value'])):
+                        params_logK[name+':'+str(n)] = prior['value'][n]
+                else: params_logK[name] = prior['value']
+            else:
+                if prior['fit'] == 'local':
+                    for n in range(len(prior['value'])):
+                        params_kcat[name+':'+str(n)] = prior['value'][n]
+                else: params_kcat[name] = prior['value']
+
+    for n, params in enumerate(params_kcat):
+        if params is None:
+            params_kcat[n] = 0.
+
+    return params_logK, params_kcat
