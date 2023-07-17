750d090e3bfbabf6f2807d73bc5b969bcfae081f
Van La <langocthuyvan@gmail.com>
2023-07-17 16:23:27 -0500

    Running model with constraints

---
+++ global_fitting/1.constraints_both/run_me.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+export SCRIPT="/ocean/projects/mcb160011p/sophie92/python/mpro/scripts/submit_mcmc_constraints.py"
+
+export OUT_DIR="/ocean/projects/mcb160011p/sophie92/python/mpro/1.constraints_both"
+
+export N_INTER=10000
+
+export N_BURN=2000
+
+export N_CHAIN=4
+
+python $SCRIPT --out_dir $OUT_DIR --niters $N_INTER --nburn $N_BURN --nchain $N_CHAIN --fit_mutant_kinetics --fit_mutant_AUC --fit_mutant_ICE --fit_wildtype_Nashed --fit_E_S --fit_E_I --multi_var_wt
