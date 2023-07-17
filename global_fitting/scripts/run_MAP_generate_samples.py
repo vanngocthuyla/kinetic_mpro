3af7675339e1fa4de1a0eaf5d5f94162e0341d5d
Van La <langocthuyvan@gmail.com>
2023-07-17 16:26:59 -0500

    Generating samples from MAP of model

--- global_fitting/scripts/run_MAP_generate_samples.py
+++ global_fitting/scripts/run_MAP_generate_samples.py
@@ -47,6 +47,8 @@ parser.add_argument( "--multi_var_wt",                  action="store_true",    
 
 parser.add_argument( "--set_K_I_M_equal_K_S_M",         action="store_true",    default=False)
 parser.add_argument( "--set_K_S_DI_equal_K_S_DS",       action="store_true",    default=False)
+parser.add_argument( "--set_kcat_DSS_equal_kcat_DS",    action="store_true",    default=False)
+parser.add_argument( "--set_kcat_DSI_equal_kcat_DS",    action="store_true",    default=False)
 parser.add_argument( "--set_kcat_DSI_equal_kcat_DSS",   action="store_true",    default=False)
 
 parser.add_argument( "--niters",                        type=int,               default=10000)
@@ -77,7 +79,7 @@ logKd_max = 0.
 kcat_min = 0. 
 
 prior = {}
-prior['logKd'] = {'type':'logKd', 'name': 'logKd', 'fit':'local', 'dist': ['normal', 'normal'], 'loc': [-5, -14], 'scale': [2, 2]}
+prior['logKd'] = {'type':'logKd', 'name': 'logKd', 'fit':'local', 'dist': ['uniform', 'normal', None], 'loc': [None, -14, None], 'scale': [None, 2, None], 'lower': [-20, None, None], 'upper': [0, None, None], 'value': None}
 prior['logK_S_M'] = {'type':'logK', 'name': 'logK_S_M', 'fit':'global', 'dist': 'uniform', 'lower': logKd_min, 'upper': logKd_max}
 prior['logK_S_D'] = {'type':'logK', 'name': 'logK_S_D', 'fit':'global', 'dist': 'uniform', 'lower': logKd_min, 'upper': logKd_max}
 prior['logK_S_DS'] = {'type':'logK', 'name': 'logK_S_DS', 'fit':'global', 'dist': 'uniform', 'lower': logKd_min, 'upper': logKd_max}
@@ -87,11 +89,21 @@ prior['logK_I_DI'] = {'type':'logK', 'name': 'logK_I_DI', 'fit':'global', 'dist'
 prior['logK_S_DI'] = {'type':'logK', 'name': 'logK_S_DI', 'fit':'global', 'dist': 'uniform', 'lower': logKd_min, 'upper': logKd_max}
 
 # prior['kcat_MS'] = {'type':'kcat', 'name': 'kcat_MS', 'fit':'global', 'dist': None, 'value': 0.}
-prior['kcat_DS'] = {'type':'kcat', 'name': 'kcat_DS', 'fit':'local', 'dist': 'uniform', 'lower': kcat_min, 'upper': [10, 300]}
-prior['kcat_DSS'] = {'type':'kcat', 'name': 'kcat_DSS', 'fit':'local', 'dist': 'uniform', 'lower': kcat_min, 'upper': [10, 300]}
-prior['kcat_DSI'] = {'type':'kcat', 'name': 'kcat_DSI', 'fit':'local', 'dist': 'uniform', 'lower': kcat_min, 'upper': [10, 300]}
+prior['kcat_DS'] = {'type':'kcat', 'name': 'kcat_DS', 'fit':'local', 'dist': 'uniform', 'lower': kcat_min, 'upper': [1, 200, 300]}
+prior['kcat_DSS'] = {'type':'kcat', 'name': 'kcat_DSS', 'fit':'local', 'dist': 'uniform', 'lower': kcat_min, 'upper': [1, 200, 300]}
+prior['kcat_DSI'] = {'type':'kcat', 'name': 'kcat_DSI', 'fit':'local', 'dist': 'uniform', 'lower': kcat_min, 'upper': [1, 200, 300]}
 
-shared_params = None
+if args.set_K_I_M_equal_K_S_M: 
+    del prior['logK_I_M']
+if args.set_K_S_DI_equal_K_S_DS: 
+    del prior['logK_S_DI']
+if args.set_kcat_DSS_equal_kcat_DS:
+    del prior['kcat_DSS']
+if args.set_kcat_DSI_equal_kcat_DS or args.set_kcat_DSI_equal_kcat_DSS:
+    del prior['kcat_DSI']
+
+shared_params = {}
+shared_params['logKd'] = {'assigned_idx': 2, 'shared_idx': 1}
 
 prior_infor = convert_prior_from_dict_to_list(prior, args.fit_E_S, args.fit_E_I)
 prior_infor_update = check_prior_group(prior_infor, len(expts))
@@ -107,11 +119,13 @@ rng_key, rng_key_ = random.split(random.PRNGKey(args.random_key))
 os.chdir(args.out_dir)
 
 init_values = pickle.load(open(args.map_file, "rb"))
+print("Initial values:", init_values)
 
 kernel = NUTS(model=adjustable_global_fitting, init_strategy=init_to_value(values=init_values))
 mcmc = MCMC(kernel, num_warmup=args.nburn, num_samples=args.niters, num_chains=args.nchain, progress_bar=True)
 mcmc.run(rng_key_, experiments=expts, prior_infor=prior_infor_update, shared_params=shared_params, 
          set_K_I_M_equal_K_S_M=args.set_K_I_M_equal_K_S_M, set_K_S_DI_equal_K_S_DS=args.set_K_S_DI_equal_K_S_DS, 
+         set_kcat_DSS_equal_kcat_DS=args.set_kcat_DSS_equal_kcat_DS, set_kcat_DSI_equal_kcat_DS=args.set_kcat_DSI_equal_kcat_DS,
          set_kcat_DSI_equal_kcat_DSS=args.set_kcat_DSI_equal_kcat_DSS)
 mcmc.print_summary()
 
@@ -155,21 +169,20 @@ if shared_params is not None and len(shared_params)>0:
         shared_idx = param['shared_idx']
         trace[f'{name}:{assigned_idx}'] = trace[f'{name}:{shared_idx}']
 
-[map_index, map_params, log_probs] = map_finding(trace, expts, prior_infor_update)
-
-with open("map.txt", "w") as f:
-    print("MAP index:" + str(map_index), file=f)
-    print("\nKinetics parameters:", file=f)
-    for key in trace.keys():
-        print(key, ': %.3f' %trace[key][map_index], file=f)
-
-map_values = {}
-for key in trace.keys():
-    map_values[key] = trace[key][map_index]
-pickle.dump(map_values, open('map.pickle', "wb"))
-
 ## Fitting plot
-params_logK, params_kcat = extract_params_from_map_and_prior(trace, map_index, prior_infor_update)
+params_logK, params_kcat = extract_params_from_trace_and_prior(trace, prior_infor_update)
+params_logK['logKd:2'] = params_logK['logKd:1']
+
+if args.set_K_I_M_equal_K_S_M:
+    params_logK['logK_I_M'] = params_logK['logK_S_M']
+if args.set_K_S_DI_equal_K_S_DS:
+    params_logK['logK_S_DI'] = params_logK['logK_S_DS']
+if args.set_kcat_DSS_equal_kcat_DS: 
+    params_kcat['kcat_DSS'] = params_kcat['kcat_DS']
+if args.set_kcat_DSI_equal_kcat_DS: 
+    params_kcat['kcat_DSI'] = params_kcat['kcat_DS']
+elif args.set_kcat_DSI_equal_kcat_DSS:
+    params_kcat['kcat_DSI'] = params_kcat['kcat_DSS']
 
 n = 0
 for expt_plot in [expts_mut, expts_wt, expts_wt_2]:
