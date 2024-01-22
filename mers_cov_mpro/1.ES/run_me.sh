b887f585540520172eb25a4273698dbefae15415
Van La <langocthuyvan@gmail.com>
2024-01-22 00:43:51 -0600

    Global fitting - Step 1

--- mers_cov_mpro/1.ES/run_me.sh
+++ mers_cov_mpro/1.ES/run_me.sh
@@ -6,10 +6,10 @@ export INPUT="/home/exouser/python/mers_mpro/input/ES_mean.csv"
 
 export OUT_DIR="/home/exouser/python/mers_mpro/1.ES"
 
-export N_INTER=100
+export N_INTER=1000
 
-export N_BURN=20
+export N_BURN=200
 
 export N_CHAIN=4
 
-python $SCRIPT --input_file $INPUT --out_dir $OUT_DIR --niters $N_INTER --nburn $N_BURN --nchain $N_CHAIN --fit_E_S --set_error_E
+python $SCRIPT --input_file $INPUT --out_dir $OUT_DIR --niters $N_INTER --nburn $N_BURN --nchain $N_CHAIN --fit_E_S --set_lognormal_dE --dE 0.5
