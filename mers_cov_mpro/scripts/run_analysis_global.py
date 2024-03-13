f24f9f98f0e31e929d3ddb20fe76d0ca178d2419
Van La <langocthuyvan@gmail.com>
2024-03-12 22:54:04 -0500

    Update run_analysis_global.py

--- mers_cov_mpro/scripts/run_analysis_global.py
+++ mers_cov_mpro/scripts/run_analysis_global.py
@@ -8,19 +8,15 @@ import argparse
 import pickle
 import arviz as az
 import pandas as pd
-from scipy import stats
-
-import seaborn as sns
-import matplotlib.pyplot as plt
 
 import jax
 import jax.numpy as jnp
 
 from _load_data_mers import load_data_one_inhibitor
-from _plotting import plot_data_conc_log
-
+from _prior_check import convert_prior_from_dict_to_list, check_prior_group
 from _params_extraction import extract_logK_n_idx, extract_kcat_n_idx
 from _trace_analysis import extract_params_from_map_and_prior, _trace_ln_to_log
+from _plotting import plot_data_conc_log, _plot_1D_histogram, _plot_2D_histogram, _heat_map, _linear_corr
 
 warnings.simplefilter(action='ignore', category=FutureWarning)
 warnings.simplefilter("ignore", UserWarning)
@@ -30,7 +26,7 @@ parser = argparse.ArgumentParser()
 
 parser.add_argument( "--kinetic_file",                  type=str,               default="")
 parser.add_argument( "--mcmc_file",                     type=str,               default="")
-parser.add_argument( "--mcmc_dir",                      type=str,               default="")
+parser.add_argument( "--map_dir",                       type=str,               default="")
 parser.add_argument( "--out_dir",                       type=str,               default="")
 parser.add_argument( "--name_inhibitor",                type=str,               default="")
 
@@ -41,6 +37,7 @@ parser.add_argument( "--dE",                            type=float,             
 
 parser.add_argument( "--nchain",                        type=int,               default=4)
 parser.add_argument( "--niters",                        type=int,               default=0)
+parser.add_argument( "--extended_global",               type=int,               default=False)
 
 args = parser.parse_args()
 
@@ -63,64 +60,39 @@ if args.niters == 0:
 else:
     niters = args.niters
 
-# 1D Histogram
-def _plot_1D_histogram(trace, param_name, outfile, text_size=24, dpi=150):
-    trace_plot = {}
-    for key in trace.keys():
-        if key.startswith(param_name):
-            trace_plot[key] = trace[key]
+## --------------------------
+#Plot of shared parameters
+params_names = {'kcat_DS':              '$k_{cat,DS}$',
+                'kcat_DSS':             '$k_{cat,DSS}$',
+                'logKd':                '$logK_d$',
+                'logK_S_M':             '$logK_{S,M}$',
+                'logK_S_D':             '$logK_{S,D}$',
+                'logK_S_DS':            '$logK_{S,DS}$'}
 
-    az.plot_posterior(trace_plot, textsize=text_size);
-    plt.savefig(outfile, dpi=dpi, bbox_inches='tight')
+# Change parameter names for plotting
+trace_log_plot = {}
+for key in trace_log.keys():
+    if key in params_names.keys():
+        trace_log_plot[params_names[key]] = trace_log[key]
+pickle.dump(trace_log_plot, open(os.path.join(args.out_dir, "traces_log_plot_logK_S.pickle"), "wb"))
 
-def _plot_2D_histogram(trace, param_name, outfile, text_size=40, dpi=150):
-    plt.figure()
-    traces = {}
-    for key in trace.keys():
-        if key.startswith(param_name):
-            traces[key] = np.reshape(trace[key], (args.nchain, niters))
-    data = az.convert_to_inference_data(traces)
-    az.plot_pair(data, kind='kde', textsize=text_size, divergences=True)
-    plt.savefig(outfile, dpi=dpi, bbox_inches='tight');
+param_list = ['$logK_d$', '$logK_{S,M}$', '$logK_{S,D}$', '$logK_{S,DS}$']
+_plot_1D_histogram(trace_log_plot, param_list, os.path.join(args.out_dir, '1D_logK'))
 
-def _heat_map(trace, param_name, outfile, dpi=150):
-    plt.figure()
-    traces = {}
-    for key in trace.keys():
-        if key.startswith(param_name):
-            traces[key] = np.reshape(trace[key], (args.nchain, niters))
-    data = az.convert_to_inference_data(traces)
-    data_plot = az.InferenceData.to_dataframe(data)
-    data_plot = data_plot.drop(['chain', 'draw'], axis=1)
-    sns.heatmap(data_plot.corr(), annot=True, fmt="0.2f", linewidth=.5)
-    plt.tight_layout()
-    plt.savefig(outfile, dpi=dpi, bbox_inches='tight');
-
-
-#Linear correlation
-def _linear_corr(xdata, ydata, xlabel, ylabel, outfile=None, fontsize_tick=18, fontsize_text=12):
-    plt.figure()
-    ax = plt.axes()
-    res = stats.linregress(xdata, ydata)
-    ax.plot(xdata, ydata, 'o', label='data')
-    ax.plot(xdata, res.intercept + res.slope*xdata, 'k', label='Fitted line')
-    ax.set_xlabel(xlabel, fontsize=fontsize_tick)
-    ax.set_ylabel(ylabel, fontsize=fontsize_tick)
-    ax.text(0.2, 0.9, ylabel+'='+str(round(res.slope, 2))+'*'+xlabel+'+'+str(round(res.intercept, 2)),
-            fontsize=fontsize_text, transform=ax.transAxes);
-    plt.tight_layout()
-    if outfile is not None:
-        plt.savefig(outfile, bbox_inches='tight');
-
+param_list = ['$k_{cat,DS}$', '$k_{cat,DSS}$']
+_plot_1D_histogram(trace_log_plot, param_list, os.path.join(args.out_dir, '1D_kcat'))
+## --------------------------
 #Plot of alpha and dE
-params_names = {'alpha:E100_S1350':  '$\\alpha^{E:100nM, S:1350nM}$',
-                'alpha:E100_S50':    '$\\alpha^{E:100nM, S:50nM}$',
-                'alpha:E100_S750':   '$\\alpha^{E:100nM, S:750nM}$',
-                'alpha:E50_S150':    '$\\alpha^{E:50nM, S:150nM}$',
-                'dE:100':       '$[E]^{100nM}$',
-                'dE:50':        '$[E]^{50nM}$',
-                'dE:25':        '$[E]^{25nM}$',
-                }
+params_names = {}
+for _key in trace.keys():
+    if _key.startswith("alpha"):
+        _new_key = r'{'+_key[6:]+'}'
+        _new_key =_new_key.replace("_", "-")
+        params_names[_key] = f'$\\alpha^{_new_key}$'
+    if _key.startswith("dE"):
+        _new_key = r'{'+_key[3:]+'}'
+        _new_key =_new_key.replace("_", "-")
+        params_names[_key] = f'$[E]^{_new_key}$'
 
 # Change parameter names for plotting
 trace_log_plot = {}
@@ -129,26 +101,34 @@ for key in trace_log.keys():
         trace_log_plot[params_names[key]] = trace_log[key]
 pickle.dump(trace_log_plot, open(os.path.join(args.out_dir, "traces_log_plot_alpha_dE.pickle"), "wb"))
 
-_plot_1D_histogram(trace_log_plot, '$\\alpha', os.path.join(args.out_dir, '1D_alpha'), text_size=28)
-_plot_2D_histogram(trace_log_plot, '$\\alpha', os.path.join(args.out_dir, '2D_alpha'), text_size=24)
-_heat_map(trace_log_plot, '$\\alpha', os.path.join(args.out_dir, 'heatmap_alpha'))
+if args.extended_global:
+    param_name = '$\\alpha'
+    param_list = [key for key in trace_log_plot.keys() if key.startswith(param_name)]
+else:
+    param_list = ['$\\alpha^{E100-S1350}$', '$\\alpha^{E100-S750}$', '$\\alpha^{E100-S50}$', '$\\alpha^{E50-S150}$']
+_plot_1D_histogram(trace_log_plot, param_list, os.path.join(args.out_dir, '1D_alpha'))
+_plot_2D_histogram(trace_log_plot, param_list, os.path.join(args.out_dir, '2D_alpha'), label_size=20, tick_size=18, rotation_x=90)
+_heat_map(trace_log_plot, param_list, os.path.join(args.out_dir, 'heatmap_alpha'), dpi=200,
+          fig_size=(4.25, 3.75), fontsize_tick=18, nchain=args.nchain, niters=niters)
 
-_plot_1D_histogram(trace_log_plot, '$[E]', os.path.join(args.out_dir, '1D_dE'), text_size=18)
-_plot_2D_histogram(trace_log_plot, '$[E]', os.path.join(args.out_dir, '2D_dE'), text_size=24)
-_heat_map(trace_log_plot, '$[E]', os.path.join(args.out_dir, 'heatmap_dE'))
+param_list = ['$[E]^{100}$', '$[E]^{50}$', '$[E]^{25}$']
+_plot_1D_histogram(trace_log_plot, param_list, os.path.join(args.out_dir, '1D_dE'))
+_plot_2D_histogram(trace_log_plot, param_list, os.path.join(args.out_dir, '2D_dE'), label_size=24, tick_size=22, rotation_x=90)
+_heat_map(trace_log_plot, param_list, os.path.join(args.out_dir, 'heatmap_dE'), dpi=200,
+          fig_size=(3.25, 2.75), fontsize_tick=14, nchain=args.nchain, niters=niters)
 
+## --------------------------
 # MAP index extraction
-with open(os.path.join(args.mcmc_dir, 'map.txt')) as f:
+with open(os.path.join(args.map_dir, 'map.txt')) as f:
     first_line = f.readline()
 map_index = int(first_line.replace("\n", "").split(":")[1])
 
 # Load prior file
-df = pd.read_csv(os.path.join(args.mcmc_dir, 'Prior_infor.csv'))
-prior_infor_update = []
-for index, row in df.iterrows():
-    prior_infor_update.append(row.to_dict())
+prior = pickle.load(open(os.path.join(args.map_dir, 'MAP_prior.pickle'), "rb"))
+prior_infor = convert_prior_from_dict_to_list(prior, True, True)
+prior_infor_update = check_prior_group(prior_infor, len(inhibitor_list))
 
-map_sampling = pickle.load(open(os.path.join(args.mcmc_dir, 'map.pickle'), "rb"))
+map_sampling = pickle.load(open(os.path.join(args.map_dir, 'map.pickle'), "rb"))
 alpha_list = {}
 for key in map_sampling.keys():
     if key.startswith('alpha'):
@@ -160,8 +140,9 @@ if args.set_lognormal_dE and args.dE>0:
         E_list[key] = map_sampling[key]
 else: E_list = None
 
+## --------------------------
 for idx, inhibitor_name in enumerate(inhibitor_list):
-    _dir = inhibitor_name[8:12]
+    _dir = inhibitor_name[7:12]
 
     print("Woking with "+inhibitor_name)
     if not os.path.exists(os.path.join(args.out_dir, _dir)):
@@ -184,11 +165,12 @@ for idx, inhibitor_name in enumerate(inhibitor_list):
                     'logKd':                '$logK_d$',
                     'log_sigma:ES'  :       'log$\sigma^{ES}$',
                     'log_sigma:ES:0':       'log$\sigma^{ES}$',
-                    f'log_sigma:{_dir}:0':  'log$\sigma^{E:100nM, S:50nM}$',
-                    f'log_sigma:{_dir}:1':  'log$\sigma^{E:100nM, S:50nM}$',
-                    f'log_sigma:{_dir}:2':  'log$\sigma^{E:100nM, S:750nM}$',
-                    f'log_sigma:{_dir}:3':  'log$\sigma^{E:50nM, S:150nM}$'
                     }
+    
+    for i in range(len(expts_plot)):
+        log_sigma_name = expts_plot[i]['plate']
+        log_sigma_name = log_sigma_name.replace("_", "-")
+        params_names[f'log_sigma:{_dir}:{i}'] = f'log$\sigma^{log_sigma_name}$'
 
     # Change parameter names for plotting
     trace_plot = {}
@@ -199,28 +181,73 @@ for idx, inhibitor_name in enumerate(inhibitor_list):
             trace_log_plot[params_names[key]] = trace_log[key]
     pickle.dump(trace_log_plot, open(os.path.join(args.out_dir, _dir, "traces_log_plot.pickle"), "wb"))
         
+    ## --------------------------
     ## Fitting plot
     params_logK, params_kcat = extract_params_from_map_and_prior(trace_plot, map_index, prior_infor_update)
 
     n = idx+1
-    plot_data_conc_log(expts_plot, extract_logK_n_idx(params_logK, n, shared_params),
-                       extract_kcat_n_idx(params_kcat, n, shared_params),
-                       alpha_list=alpha_list, E_list=E_list,
-                       OUTFILE=os.path.join(args.out_dir, _dir, 'ESI'))
+    if args.extended_global:
+        if idx == 11:
+            plot_data_conc_log(expts_plot, extract_logK_n_idx(params_logK, n, shared_params),
+                              extract_kcat_n_idx(params_kcat, n, shared_params),
+                              alpha_list=alpha_list, E_list=E_list, fontsize_tick=15, fontsize_label=20,
+                              line_colors=['deeppink', 'blue', 'green', 'orange', 'purple'],
+                              fig_size=(6, 4), plot_legend=False, dpi=100,
+                              OUTFILE=os.path.join(args.out_dir, _dir, 'ESI'))
+        elif idx == 13:
+            plot_data_conc_log(expts_plot, extract_logK_n_idx(params_logK, n, shared_params),
+                              extract_kcat_n_idx(params_kcat, n, shared_params),
+                              alpha_list=alpha_list, E_list=E_list, fontsize_tick=15, fontsize_label=20,
+                              line_colors=['olive'], fig_size=(6, 4), plot_legend=False, dpi=100,
+                              OUTFILE=os.path.join(args.out_dir, _dir, 'ESI'))
+        elif idx == 14:
+            plot_data_conc_log(expts_plot[start:end], extract_logK_n_idx(params_logK, n, shared_params),
+                              extract_kcat_n_idx(params_kcat, n, shared_params),
+                              alpha_list=alpha_list, E_list=E_list, fontsize_tick=15, fontsize_label=20,
+                              line_colors=['gray'],fig_size=(6, 4), plot_legend=False, dpi=100,
+                              OUTFILE=os.path.join(args.out_dir, _dir, 'ESI'))
+        else:
+            plot_data_conc_log(expts_plot, extract_logK_n_idx(params_logK, n, shared_params),
+                               extract_kcat_n_idx(params_kcat, n, shared_params),
+                               alpha_list=alpha_list, E_list=E_list,
+                               fig_size=(6, 4), plot_legend=False, dpi=100,
+                               OUTFILE=os.path.join(args.out_dir, _dir, 'ESI'))
+    else:
+        plot_data_conc_log(expts_plot, extract_logK_n_idx(params_logK, n, shared_params),
+                           extract_kcat_n_idx(params_kcat, n, shared_params),
+                           alpha_list=alpha_list, E_list=E_list,
+                           fig_size=(6, 4), plot_legend=False, dpi=100,
+                           OUTFILE=os.path.join(args.out_dir, _dir, 'ESI'))
 
-    _plot_1D_histogram(trace_log_plot, '$logK_', os.path.join(args.out_dir, _dir, '1D_logK'), text_size=24)
-    _plot_2D_histogram(trace_log_plot, '$logK_', os.path.join(args.out_dir, _dir, '2D_logK'))
-    _heat_map(trace_log_plot, '$logK_', os.path.join(args.out_dir, _dir, 'heatmap_logK'))
+    ## --------------------------
+    param_list = ['$logK_{I,M}$', '$logK_{I,D}$', '$logK_{I,DI}$', '$logK_{S,DI}$']
+    _plot_1D_histogram(trace_log_plot, param_list, os.path.join(args.out_dir, _dir, '1D_logK_EI'))
+    param_list = ['$logK_d$', '$logK_{S,M}$', '$logK_{S,D}$', '$logK_{S,DS}$',
+                  '$logK_{I,M}$', '$logK_{I,D}$', '$logK_{I,DI}$', '$logK_{S,DI}$']
+    _plot_1D_histogram(trace_log_plot, param_list, os.path.join(args.out_dir, _dir, '1D_logK'))
+    _plot_2D_histogram(trace_log_plot, param_list, label_size=24, tick_size=22, rotation_x=90, 
+                       outfile=os.path.join(args.out_dir, _dir, '2D_logK'))
+    _heat_map(trace_log_plot, param_list, outfile=os.path.join(args.out_dir, _dir, 'heatmap_logK'),
+              nchain=args.nchain, niters=niters)
 
-    _plot_1D_histogram(trace_log_plot, '$k_{cat', os.path.join(args.out_dir, _dir, '1D_kcat'), text_size=32)
-    _plot_2D_histogram(trace_log_plot, '$k_{cat', os.path.join(args.out_dir, _dir, '2D_kcat'))
-    _heat_map(trace_log_plot, '$k_{cat', os.path.join(args.out_dir, _dir, 'heatmap_kcat'))
+    ## --------------------------
+    param_name = '$k_{cat'
+    param_list = [key for key in trace_log_plot.keys() if key.startswith(param_name)]
+    _plot_1D_histogram(trace_log_plot, param_list, outfile=os.path.join(args.out_dir, _dir, '1D_kcat'))
+    _plot_2D_histogram(trace_log_plot, param_list, outfile=os.path.join(args.out_dir, _dir, '2D_kcat'))
+    _heat_map(trace_log_plot, param_list, outfile=os.path.join(args.out_dir, _dir, 'heatmap_kcat'), dpi=200,
+              fig_size=(3.25, 2.75), fontsize_tick=14, nchain=args.nchain, niters=niters)
 
+    ## --------------------------
     for key in trace_log.keys():
         if key.startswith('logK_I_DI'): logK_I_DI = trace_log[key]
         if key.startswith('logK_S_DI'): logK_S_DI = trace_log[key]
         if key.startswith('logK_I_D') and not key.startswith('logK_I_DI'): logK_I_D  = trace_log[key]
+    
+    _linear_corr(logK_I_D, logK_S_DI, 'log$K_{I,D}$', 'log$K_{S,DI}$', os.path.join(args.out_dir, _dir, 'Linear_Correlation_1'))
+    _linear_corr(logK_I_D, logK_I_DI, 'log$K_{I,D}$', 'log$K_{I,DI}$', os.path.join(args.out_dir, _dir, 'Linear_Correlation_2'))
+    _linear_corr(logK_S_DI, logK_I_DI, 'log$K_{S,DI}$', 'log$K_{I,DI}$', os.path.join(args.out_dir, _dir, 'Linear_Correlation_3'))
 
-    _linear_corr(logK_I_DI, logK_S_DI, 'log$K_{I,DI}$', 'log$K_{S,DI}$', os.path.join(args.out_dir, _dir, 'Linear_Correlation_1'))
-    _linear_corr(logK_S_DI, logK_I_D, 'log$K_{S,DI}$', 'log$K_{I,D}$', os.path.join(args.out_dir, _dir, 'Linear_Correlation_2'))
-    _linear_corr(logK_I_DI, logK_I_D, 'log$K_{I,DI}$', 'log$K_{I,D}$', os.path.join(args.out_dir, _dir, 'Linear_Correlation_3'))
+    _linear_corr(logK_I_D, logK_S_DI, 'log$K_{I,D}$', 'log$K_{S,DI}$', os.path.join(args.out_dir, _dir, 'Linear_Correlation_1_Text'), legend=True)
+    _linear_corr(logK_I_D, logK_I_DI, 'log$K_{I,D}$', 'log$K_{I,DI}$', os.path.join(args.out_dir, _dir, 'Linear_Correlation_2_Text'), legend=True)
+    _linear_corr(logK_S_DI, logK_I_DI, 'log$K_{S,DI}$', 'log$K_{I,DI}$', os.path.join(args.out_dir, _dir, 'Linear_Correlation_3_Text'), legend=True)
