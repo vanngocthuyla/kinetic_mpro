1260f5dbb3f139642c9dac8d99a77b7a6e39fc8e
Van La <langocthuyvan@gmail.com>
2024-07-22 18:23:21 -0500

    Example of fitting one CRC

---
+++ mpro/CRC/run_me.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+export SCRIPT="/home/exouser/python/mpro/scripts/submit_CRC.py"
+
+export RUNNING_SCRIPT="/home/exouser/python/mpro/scripts/run_CRC_fitting.py"
+
+export INPUT="/home/exouser/python/mpro/CRC/input/CDD_20240222_normalized_data.csv"
+
+export PRIOR='/home/exouser/python/mpro/CRC/input/Prior.json'
+
+export MAP_FILE="/home/exouser/python/mpro/CRC/input/map_sampling.pickle"
+
+export OUT_DIR="/home/exouser/python/mpro/CRC/output"
+
+export N_INTER=1000
+
+export N_BURN=200
+
+export N_CHAIN=4
+
+export NAME='ASAP-0014973'
+
+python $SCRIPT --input_file $INPUT --out_dir $OUT_DIR --running_script $RUNNING_SCRIPT --prior_infor $PRIOR --niters $N_INTER --nburn $N_BURN --nchain $N_CHAIN --fit_E_S --fit_E_I --set_lognormal_dE --dE 0.1 --multi_var --name_inhibitor "$NAME" --map_file $MAP_FILE --outlier_removal
