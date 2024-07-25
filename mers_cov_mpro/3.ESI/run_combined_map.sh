617a2f64a76423055d3e522e0a062edce225837c
Van La <langocthuyvan@gmail.com>
2024-07-25 16:30:02 -0500

    Combine all analyses into one.

--- mers_cov_mpro/3.ESI/run_combined_map.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-export SCRIPT="/home/exouser/python/mers_mpro/scripts/run_MAP_combining.py"
-
-export MCMC_DIR="/home/exouser/python/mers_mpro/2.ESI"
-
-export OUT_DIR="/home/exouser/python/mers_mpro/3.ESI"
-
-python $SCRIPT --mcmc_dir $MCMC_DIR --out_dir $OUT_DIR
