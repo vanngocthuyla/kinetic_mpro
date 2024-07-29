cda03502c55f9f46cac58a44842b316026004f1c
Van La <langocthuyvan@gmail.com>
2024-07-29 12:24:52 -0500

    Update run_me.sh

--- CRC_pIC50/run_me.sh
+++ CRC_pIC50/run_me.sh
@@ -18,6 +18,6 @@ export N_BURN=200
 
 export N_CHAIN=4
 
-export NAME='14973'
+export NAME='ID_14973'
 
 python $SCRIPT --input_file $INPUT --out_dir $OUT_DIR --running_script $RUNNING_SCRIPT --prior_infor $PRIOR --niters $N_INTER --nburn $N_BURN --nchain $N_CHAIN --fit_E_S --fit_E_I --set_lognormal_dE --dE 0.1 --multi_var --name_inhibitor "$NAME" --map_file $MAP_FILE --outlier_removal --exclude_first_trace
