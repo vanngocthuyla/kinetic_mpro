8c1f87682e093d4fd8a7d3bf7b6ebd2ccbba654e
Van La <langocthuyvan@gmail.com>
2024-07-30 12:04:47 -0500

    Update to run fitting for SARS-CoV-2 MPro

--- sars/1.global/run_me.sh
+++ sars/1.global/run_me.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
-export SCRIPT="/home/exouser/python/mpro/scripts/run_mcmc_sars.py"
+export DIR='/ocean/projects/mcb160011p/sophie92/python'
 
-export OUT_DIR="/home/exouser/python/mpro/sars/1.global"
+export SCRIPT='/kinetic_mpro/scripts/run_mcmc_sars.py'
 
-export PRIOR='/home/exouser/python/mpro/sars/1.global/Prior.json'
+export OUT_DIR='/kinetic_mpro/sars/1.global'
 
-export N_INTER=1000
+export PRIOR='/kinetic_mpro/sars/1.global/Prior.json'
 
-export N_BURN=200
+export N_INTER=10000
+
+export N_BURN=2000
 
 export N_CHAIN=4
 
-python $SCRIPT --out_dir $OUT_DIR --prior_infor $PRIOR --niters $N_INTER --nburn $N_BURN --nchain $N_CHAIN  --fit_mutant_kinetics  --fit_mutant_AUC  --fit_mutant_ICE  --fit_wildtype_Nashed  --fit_E_S  --fit_E_I  --multi_var_wt
+python $DIR$SCRIPT --out_dir $DIR$OUT_DIR --prior_infor $DIR$PRIOR --niters $N_INTER --nburn $N_BURN --nchain $N_CHAIN  --fit_mutant_kinetics  --fit_mutant_AUC  --fit_mutant_ICE  --fit_wildtype_Nashed  --fit_E_S  --fit_E_I  --multi_var_wt
---
+++ sars/1.global/run_me_submit.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+export DIR='/ocean/projects/mcb160011p/sophie92/python'
+
+export SCRIPT='/kinetic_mpro/scripts/submit_sars_2.py'
+
+export RUNNING_SCRIPT='/kinetic_mpro/scripts/run_mcmc_sars.py'
+
+export OUT_DIR='/kinetic_mpro/sars/1.global'
+
+export PRIOR='/kinetic_mpro/sars/1.global/Prior.json'
+
+export N_INTER=10000
+
+export N_BURN=2000
+
+export N_CHAIN=4
+
+python $DIR$SCRIPT --running_script $DIR$RUNNING_SCRIPT --out_dir $DIR$OUT_DIR --prior_infor $DIR$PRIOR --niters $N_INTER --nburn $N_BURN --nchain $N_CHAIN --fit_mutant_kinetics  --fit_mutant_AUC  --fit_mutant_ICE  --fit_wildtype_Nashed  --fit_E_S  --fit_E_I  --multi_var_wt
