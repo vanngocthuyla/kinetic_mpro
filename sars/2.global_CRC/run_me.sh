fab631953f42ac4714337ff931ef5a0627760190
Van La <langocthuyvan@gmail.com>
2024-08-01 22:51:31 -0500

    Update run_me.sh

--- sars/2.global_CRC/run_me.sh
+++ sars/2.global_CRC/run_me.sh
@@ -4,16 +4,20 @@ export DIR='/home/exouser/python'
 
 export SCRIPT='/kinetic_mpro/scripts/run_mcmc_sars_CRC.py'
 
+export INPUT='/kinetic_mpro/sars/input/SARS_2_CRC.csv'
+
 export OUT_DIR='/kinetic_mpro/sars/2.global_CRC'
 
 export PRIOR='/kinetic_mpro/sars/2.global_CRC/Prior.json'
 
 export SHARED_PARAMS='/kinetic_mpro/sars/2.global_CRC/Shared_params.json'
 
-export N_INTER=500
+export N_INTER=10000
 
-export N_BURN=20
+export N_BURN=2000
 
 export N_CHAIN=4
 
-python $DIR$SCRIPT --out_dir $DIR$OUT_DIR --prior_infor $DIR$PRIOR --shared_params_infor $DIR$SHARED_PARAMS --niters $N_INTER --nburn $N_BURN --nchain $N_CHAIN  --fit_mutant_kinetics  --fit_mutant_AUC  --fit_mutant_ICE  --fit_wildtype_Nashed  --fit_E_S  --fit_E_I  --multi_var_wt --multi_var_CRC 
+export NAME='ID_20650'
+
+python $DIR$SCRIPT --input_file $DIR$INPUT --out_dir $DIR$OUT_DIR --prior_infor $DIR$PRIOR --shared_params_infor $DIR$SHARED_PARAMS --name_inhibitor "$NAME" --niters $N_INTER --nburn $N_BURN --nchain $N_CHAIN  --fit_mutant_kinetics  --fit_mutant_AUC  --fit_mutant_ICE  --fit_wildtype_Nashed  --fit_E_S  --fit_E_I  --multi_var_wt --multi_var_CRC 
